4bb3254948e93e9eae6c9da97fe35e7dd08cd45a
Slavic <serghienco.v@gmail.com>
2014-05-25 00:09:44 -0700

    created root project and algorithms project

---
+++ root/algorithms/src/main/java/com/slavic/algorithms/App.java
@@ -0,0 +1,13 @@
+package com.slavic.algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
