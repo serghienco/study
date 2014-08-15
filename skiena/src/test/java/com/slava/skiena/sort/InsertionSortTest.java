fe95608b54928f74ff047f67799eff4a6778bf59
Veaceslav Serghienco <vserghienco@tidemark.com>
2014-08-14 19:06:54 -0700

    slecting the right job task

--- skiena/src/test/java/com/slava/skiena/sort/InsertionSortTest.java
+++ skiena/src/test/java/com/slava/skiena/sort/InsertionSortTest.java
@@ -11,7 +11,7 @@ public class InsertionSortTest {
     private int[] expectedList = {0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
 
     @Test
-    public void test() {
+    public void testInsertionSort() throws Exception {
         int[] list = {1, 4, 5, 0, 6, 7, 8, 3, 5, 9, 2};
         InsertionSort.sort(list);
         Assert.assertArrayEquals(expectedList, list);
---
+++ skiena/src/test/java/com/slava/skiena/sort/com/slava/skiena/SelectingTheRightJobTest.java
@@ -0,0 +1,29 @@
+package com.slava.skiena.sort.com.slava.skiena;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by vserghienco on 8/14/14.
+ */
+public class SelectingTheRightJobTest {
+
+    @Test
+    public void testGetTheRightJobs() throws Exception {
+        List<SelectingTheRightJob.Job> expectedJobs = Lists.newArrayList(
+                new SelectingTheRightJob.Job("The President's Algorist", 1, 20),
+                new SelectingTheRightJob.Job("Discrete Mathematics", 3, 15),
+                new SelectingTheRightJob.Job("Tarjan ot the Jungle", 7, 24),
+                new SelectingTheRightJob.Job("Halting State", 19, 29),
+                new SelectingTheRightJob.Job("Steiner's Tree", 22, 33),
+                new SelectingTheRightJob.Job("The Four Volume Problem", 31, 51),
+                new SelectingTheRightJob.Job("Programming Challenges", 35, 47),
+                new SelectingTheRightJob.Job("Process Terminated", 44, 64),
+                new SelectingTheRightJob.Job("Calculated Bets", 50, 65)
+        );
+
+        System.out.print(SelectingTheRightJob.getTheRightJobs(expectedJobs));
+    }
+}
